49ce2566a5b8c2e29fee10ea8696585674972f5e
nadeemchoudhari <nadim.choudhari@gmail.com>
2022-12-15 19:21:41 +0530

    automate argocd dep from ec2-jenkins

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,12 +2,35 @@
 
 resource "aws_instance" "myjenkins" {
   ami = data.aws_ami.amzlinux2.id
+  depends_on = [ module.eks.cluster_endpoint ]
   instance_type = "t2.micro"
   key_name = var.ec2keypair
   subnet_id = "subnet-1daead75"
   vpc_security_group_ids = [data.aws_security_group.ec2sg.id]
-  user_data = file("${path.module}/install.sh")
   tags = {
     "Name" = "jenkins-demo"
   }
+
+  provisioner "file" {
+    source      = "install.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh ",
+    ]
+  
+  }
+
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./my-keypair.pem")
+      timeout     = "4m"
+   }
 }
+
+
--- install.sh
+++ install.sh
@@ -1,16 +1,35 @@
+#!/bin/bash
+
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
+#sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo yum search docker
 sudo yum info docker
-sudo yum install docker
-
+sudo yum install docker -y
+sudo yum install unzip -y
+sudo yum install wget -y
+sudo yum install git -y
 sudo usermod -a -G docker ec2-user
 sudo usermod -a -G docker jenkins
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+export AWS_ACCESS_KEY_ID="AKIASME2YHOVBYQIMLXY"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_DEFAULT_REGION="ap-south-1"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.7/2022-10-31/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/bin/
+aws eks update-kubeconfig --region ap-south-1 --name demo-eks-cluster
+git clone https://github.com/nadeemchoudhari/demo-argocd-helm.git  ~/demo-argocd-helm
+cd ~/demo-argocd-helm
+git checkout master
+kubectl create namespace argocd
+kubectl -n argocd apply -f ~/demo-argocd-helm/install.yaml
